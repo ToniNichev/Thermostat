7c4205428425c81ccf86d3c65c577eecae06a2fe
Toni Nichev <toninichev@Tonis-MacBook-Pro.local>
2021-07-01 13:01:04 -0400

    work

---
+++ receivers/receivers/RFCommunicator.cpp
@@ -0,0 +1,40 @@
+#include "RFCommunicator.h"
+
+RF24 RFCommunicatorRadio(9, 8);  // CE, CSN
+//address through which two modules communicate.
+const byte RFCommunicatorAddress[6] = "00001";
+const byte RFCommunicatorAddressNewDevice[6] = "00002";
+short int RFCommunicatorMode = 0;
+
+bool RFCommunicatorListen(char data[]) {
+  if(RFCommunicatorMode == 0) {
+    RFCommunicatorRadio.begin();
+    RFCommunicatorRadio.openReadingPipe(0, RFCommunicatorAddress);
+    RFCommunicatorRadio.startListening();   
+    Serial.println("Listening ...");
+    RFCommunicatorMode = 1; // listen
+    return false;
+  }
+  else {
+    if (RFCommunicatorRadio.available())
+    {
+      Serial.println("Data coming ...");
+      char text[100] = {0};
+      RFCommunicatorRadio.read(&text, sizeof(text));
+      strcpy(data, text);
+      RFCommunicatorMode = 0;
+      return true;
+     }
+  }
+}
+
+void RFCommunicatorSend(char sendText[]) {
+  RFCommunicatorRadio.begin();
+  RFCommunicatorRadio.stopListening();  
+  RFCommunicatorRadio.openWritingPipe(RFCommunicatorAddress);
+  //delay(100);
+
+  const char text[150] = {0};
+  strcpy(text,sendText);
+  RFCommunicatorRadio.write(&text, sizeof(text));
+}
---
+++ receivers/receivers/RFCommunicator.h
@@ -0,0 +1,12 @@
+#ifndef RFCOMMUNICATOR_H
+#define RFCOMMUNICATOR_H
+#include <Arduino.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+
+bool RFCommunicatorListen(char data[]);
+void RFCommunicatorSend(char sendText[]);
+
+
+#endif
