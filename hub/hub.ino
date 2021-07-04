f9802826a08a643c1bdab0fe82faf2f170e90a79
ToniNichev <toni.nichev@gmail.com>
2021-07-03 22:33:16 -0400

    #stable work

--- hub/hub.ino
+++ hub/hub.ino
@@ -6,42 +6,43 @@ char serverData[100] = {0};
 int len;
 short int programMode = 0;
 int loops = 0;
-char thermostatsData[100] = "";
-char ethernetURL[100] = "";      
+char thermostatsData[100] = "";      
 
 void setup() {
   Serial.begin(9600);
   RFCommunicatorSetup();
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
-  }  
+  }
+  Serial.println();
+  Serial.println();  
   Serial.println("================== PROGRAM STARTED ======================");
   delay(200);
 }
 
 void loop() {
+  char ethernetURL[100] = "";  
   switch(programMode) {
-    case 0:
+    case 0: 
       strcpy(ethernetURL, "GET /thermostat-services/get-data?data=");
       strcat(ethernetURL, thermostatsData);
       strcat(ethernetURL, " HTTP/1.1");
       Serial.print("url:");
       Serial.print(ethernetURL);
-      Serial.println();  
-      programMode = 1;    
-    case 1:    
-      if( setupEthernetWebClient("GET /thermostat-services/get-data?data=[1,2,4] HTTP/1.1", "toni-develops.com", 8061, serverData, len) == true) {
+      Serial.println(); 
+             
+      while(setupEthernetWebClient(ethernetURL, "toni-develops.com", 8061, serverData, len) == false) {
+        ; // wait untill get server data
+      }
         Serial.print("server data:");
         Serial.print(serverData);
         Serial.println();
         Serial.println();
         Serial.println();
-        programMode = 2;
-      }
-      ethernetURL[0] = '\0';
-      thermostatsData[0] = '\0';
+        programMode = 1;
+        thermostatsData[0] = '\0';
       break;
-    case 2:
+    case 1:
         Serial.print("programMode:");
         Serial.print(programMode);
         Serial.println(); 
@@ -55,13 +56,7 @@ void loop() {
         data[pos] = serverData[i];
         pos ++;
         if(serverData[i] == ']') {
-          RFCommunicatorSend(data, thermostatId);
-          
-          Serial.print(">>>>>>>>>> data : ");
-          Serial.print(data);
-          Serial.println();
-          Serial.println();
-
+          RFCommunicatorSend(data, thermostatId);          
           delay(1000);
           char temp[32] = "";
           //Serial.print("waiting for thermostat data...");
@@ -83,13 +78,12 @@ void loop() {
           //break;
         }
       }
-      programMode = 3;
-    case 3:
+      programMode = 2;
+    case 2:
       //RFCommunicatorSend(serverData);
       Serial.println("delaying 3 sec ...");
       delay(3000);
       programMode = 0;
       loops = 0;
-      ethernetURL[0] = '\0';
   }
 }
