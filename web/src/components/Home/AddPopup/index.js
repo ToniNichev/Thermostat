43655a8fc48c88bbabc8d0b93891757cdaf5fd5a
ToniNichev <toni.nichev@gmail.com>
2021-08-27 17:00:29 -0400

    Work

---
+++ web/src/components/Home/AddPopup/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import styles from './styles.scss';
+import { apiUrl } from '../../../utils/getParams';
+import EventsManager from  '../../../containers/EventsManager';
+import {Poster} from '../../../utils/Poster';
+
+const addFlag = async (closePopup) => {
+
+  //const searchReresultsult = await Poster(`${apiUrl}/find`,  {'flagName' : flag.flagName} );    
+
+  /*
+  const flag = {
+    "group": document.getElementById('addFeatureFlag').querySelector("input.group").value,
+    "flagName": document.getElementById('addFeatureFlag').querySelector("input.flagName").value ,
+    "value": document.getElementById('addFeatureFlag').querySelector("input.value").value,
+  };
+
+
+  if(searchResult.length > 0) {
+    EventsManager.callEvent('showPopup')(`Flag with name: ${flag.flagName} already exists!`);
+  }
+  else {
+    //const result = await Poster(`${apiUrl}/add`, flag);
+    closePopup();
+  }
+  */
+}
+
+
+const AddPopup = ({closePopup}) => {  
+  return (
+    <div id="addFeatureFlag" className={styles.modal}>
+      <div className={styles.modalContent}>
+        <span onClick={ () => { closePopup() } } className={styles.close}>&times;</span>
+        <div className={styles.flagProperties}>
+          <p><label>FLAG NAME</label> <input className="flagName" type="text" /></p>
+          <p><label>GROOUP</label> <input className="group" type="text" /></p>
+          <p><label>VALUE</label> <input className="value" type="text" /></p>
+          <p><button onClick={ () => { addFlag(closePopup) } }>ADD FLAG</button></p>
+        </div>          
+      </div>      
+    </div>
+  );
+}
+
+export default AddPopup;
