4e417d1959347e0d383b7aaae015f047ce4108be
ToniNichev <toni.nichev@gmail.com>
2021-09-26 12:37:47 -0400

    w

--- TestScripts/rf-send/rf-send.ino
+++ TestScripts/rf-send/rf-send.ino
@@ -1,10 +1,9 @@
 #include "RFCommunicator.h"
 #include <EEPROM.h>
-#define THERMOSTAT_EPROM_ADDRESS 3
 
 short int mode = 0;
 int q = 0;
-short int senderId;
+short int thermostatId = 0;
 short int communicationChannel;
 
 void setup() {
@@ -14,21 +13,17 @@ void setup() {
   }
   Serial.println();
   Serial.println("PROGRAM STARTED");  
-
-
-  //writeIntIntoEEPROM(THERMOSTAT_EPROM_ADDRESS, 0);  // !!!  force to set up thermostat in ADD thermostat mode !!!!
-  senderId = readIntFromEEPROM(THERMOSTAT_EPROM_ADDRESS);
-  communicationChannel = senderId + 1;
-  
-  RFCommunicatorSetup(communicationChannel, communicationChannel + 1);
 }
 
 void loop() {
+  communicationChannel = thermostatId + 1;
+  RFCommunicatorSetup(communicationChannel + 1, communicationChannel);
+    
   q ++;
   char msg[32] = {0};
-  constructMessage(senderId, q, msg);
+  constructMessage(communicationChannel, q, msg);
   RFCommunicatorSend(msg);
-
+  printToSerial(communicationChannel, msg, true);
   
   char data[32];
   RFCommunicatorListen(data, true);
@@ -42,15 +37,18 @@ void loop() {
 
 
 void constructMessage(short int senderId, int payload, char msg[32]) {
-  sprintf(msg, "%d | ⌂ >>> ⍑:  %d", senderId, payload);
+  sprintf(msg, "response( %d | ⍑ >>> ⌂:  %d)", senderId, payload);
 }
 
-void writeIntIntoEEPROM(int address, int number)
-{ 
-  EEPROM.write(address, number >> 8);
-  EEPROM.write(address + 1, number & 0xFF);
-}
-int readIntFromEEPROM(int address)
-{
-  return (EEPROM.read(address) << 8) + EEPROM.read(address + 1);
+void printToSerial(short int communicationChannel, char data[32], bool hubToThermostat) {
+  Serial.println();
+  Serial.print(communicationChannel);     
+  Serial.print(" | ");
+  if(hubToThermostat)
+    Serial.print("⌂ >>> ⍑ ");
+  else
+    Serial.print("⍑ >>> ⌂ ");
+  Serial.print(communicationChannel);
+  Serial.print(" : ");
+  Serial.print(data);
 }
