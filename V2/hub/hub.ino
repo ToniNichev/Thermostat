e5a4e5a448f8a4d01f22d2e6bb28e3c4ba3ec953
ToniNichev <toni.nichev@gmail.com>
2021-10-11 20:02:57 -0400

    work

--- V2/hub/hub.ino
+++ V2/hub/hub.ino
@@ -1,54 +1,52 @@
-#include "RFCommunicator.h"
+#include <RF24.h>
 #include <EEPROM.h>
 
 short int mode = 0;
 int q = 0;
 short int thermostatId = 0;
 short int communicationChannel;
+RF24 radio(9, 8);  // CE, CSN
+const byte addresses[][6] = {"00001", "00002", "00003", "00004"};
 
 void setup() {
+   
   Serial.begin(9600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
   Serial.println();
   Serial.println("PROGRAM STARTED");  
+
+  radio.begin();
+  radio.openWritingPipe(addresses[1]); 
+  //radio.openReadingPipe(1, addresses[1]); // 00001
+  radio.setPALevel(RF24_PA_MIN);  
+  radio.stopListening();
 }
 
 void loop() {
-  thermostatId = 0;
-  while(thermostatId < 2) {
-    // # 1 - setup
-    communicationChannel = thermostatId + 1;
-    RFCommunicatorSetup(communicationChannel + 1, communicationChannel);
-  
+
     // # 2 - send    
     q ++;
-    char msg[32] = {0};
-    constructMessage(communicationChannel, q, msg);
-    RFCommunicatorSend(msg);
-    //printToSerial(communicationChannel, msg, true);
-  
-    // # 3 - receive
-    char data[32];
-    RFCommunicatorListen(data, true);
-    printToSerial(communicationChannel, data, false);
-    //Serial.println(data);
+
+    char text[] = "this is a test 123";
+    radio.write(&text, sizeof(text));
+
   
     Serial.println();
     Serial.println("delaying 2 sec.");
     Serial.println();
     delay(2000);
-    thermostatId ++;
-  }
+    //thermostatId ++;
+  //}
 }
 
 
-void constructMessage(short int communicationChannel, int payload, char msg[32]) {
-  sprintf(msg, "(%d | ⌂ says: payload:  %d)", communicationChannel, payload);
+void constructMessage(short int communicationChannel, int payload, char msg[64]) {
+  sprintf(msg, "(%d | ⌂ 12345678910 says: payload:  %d)", communicationChannel, payload);
 }
 
-void printToSerial(short int communicationChannel, char data[32], bool hubToThermostat) {
+void printToSerial(short int communicationChannel, char data[64], bool hubToThermostat) {
   Serial.println();
   Serial.print(communicationChannel);     
   Serial.print(" | ");
