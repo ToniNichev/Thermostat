85b5dfc1a5226a2a4864e719fd1fe0d3b5d6fb8b
ToniNichev <toni.nichev@gmail.com>
2021-10-22 15:06:52 -0400

    W

--- TestScripts/oneHubMultipleThermostats/hub/hub.ino
+++ TestScripts/oneHubMultipleThermostats/hub/hub.ino
@@ -10,22 +10,37 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 RF24 radio(9, 8); // CE, CSN
-const byte address[6] = "00001";
+//const byte address[6] = "00001";
+const byte addresses[][6] = {"00001", "00010", "00020", "00025", "00030", "00035"};
+
 int q = 0;
+int id = 0;
 void setup() {
   Serial.begin(9600);
   Serial.println("Program starting ...");
   
   radio.begin();
-  radio.openWritingPipe(address);
-  radio.setPALevel(RF24_PA_MIN);
-  radio.stopListening();
+  radio.openWritingPipe(addresses[id]);
+  radio.openReadingPipe(0, addresses[id]);
+  radio.setPALevel(RF24_PA_MIN);  
 }
 void loop() {
+  
+  radio.stopListening();
   const char text[32];
   sprintf(text, "hello world %d", q);
   radio.write(&text, sizeof(text));
   Serial.println(".");
+
+  radio.startListening();
+  if (radio.available()) {
+    char text[32] = "";
+    radio.read(&text, sizeof(text));
+    Serial.println(text);
+  }
+
+  
   delay(1000);
   q ++;
+  //id = id > 2 ? 0 : id;
 }
