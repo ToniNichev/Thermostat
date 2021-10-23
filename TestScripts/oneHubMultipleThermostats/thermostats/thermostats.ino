e01c460e9065b7333128fee224885fb43bb8bb46
Toni Nichev <toni.nichev@gmail.com>
2021-10-23 13:04:20 -0400

    w

--- TestScripts/oneHubMultipleThermostats/thermostats/thermostats.ino
+++ TestScripts/oneHubMultipleThermostats/thermostats/thermostats.ino
@@ -1,6 +1,6 @@
 /*
 * Arduino Wireless Communication Tutorial
-*     Example 1 - Transmitter Code
+*       Example 1 - Receiver Code
 *                
 * by Dejan Nedelkovski, www.HowToMechatronics.com
 * 
@@ -10,38 +10,18 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 RF24 radio(9, 8); // CE, CSN
-const byte addresses[][6] = {"00001", "00010"};
-
-int q = 0;
-int id = 0;
+const byte address[6] = "00001";
 void setup() {
   Serial.begin(9600);
-  Serial.println("Program starting ...");
-  
   radio.begin();
-  radio.openWritingPipe(addresses[id]);
-  radio.openReadingPipe(0, addresses[id]);
-  radio.setPALevel(RF24_PA_MIN);  
+  radio.openReadingPipe(0, address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.startListening();
 }
 void loop() {
-
-
-  radio.startListening();
   if (radio.available()) {
     char text[32] = "";
     radio.read(&text, sizeof(text));
     Serial.println(text);
   }
-
-  
-  radio.stopListening();
-  const char text[32];
-  sprintf(text, "thermostat$d - %d", id, q);
-  radio.write(&text, sizeof(text));
-  Serial.println(".");
-
-
-  
-  delay(1000);
-  q ++;
 }
