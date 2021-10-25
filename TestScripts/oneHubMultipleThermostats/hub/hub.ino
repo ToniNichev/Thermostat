f4843370f45cd6878f49951a4f62b3dc0eaddd9a
ToniNichev <toni.nichev@gmail.com>
2021-10-25 12:14:52 -0400

    #stable - vary relieble test scripts to communicate with multiple thermostats

--- TestScripts/oneHubMultipleThermostats/hub/hub.ino
+++ TestScripts/oneHubMultipleThermostats/hub/hub.ino
@@ -14,17 +14,25 @@ const byte addresses[][6] = {"00001", "00010", "00020", "00025", "00030", "00035
 
 int q = 0;
 int id = 0;
-int chanel = (id * 2) + 1;
+
 void setup() {
   Serial.begin(9600);
   Serial.println("Program starting ...");
   
   radio.begin();
-  radio.openWritingPipe(addresses[chanel + 1]);
-  radio.openReadingPipe(0, addresses[chanel]);
   radio.setPALevel(RF24_PA_MIN);  
+  //radio.setRetries(15,15);
+  radio.setDataRate( RF24_250KBPS );
 }
 void loop() {
+  int chanel = (id * 2) + 1;
+  radio.openWritingPipe(addresses[chanel + 1]);
+  radio.openReadingPipe(0, addresses[chanel]);  
+  char msg[32];
+  sprintf(msg, "thermostat %d", id);  
+  Serial.println("=============================");
+  Serial.println(msg);
+  Serial.println("_____________________________");
 
   // SEND
   radio.stopListening();
@@ -36,17 +44,27 @@ void loop() {
 
   // RECEIVE
   radio.startListening();
-  Serial.println("Listening");
-  int listenRepeats = 0;
+  Serial.println("Listening ...");
+  int listenCycles = 0;
+  bool error = false;
   while(!radio.available()) {    
+    listenCycles ++;
+    if(listenCycles > 300) {
+      Serial.println("TIMEOUT !!!");
+      error = true;
+      break;
+    }
+    delay(10);
   }
-  char textTwo[32] = "";
-  radio.read(&textTwo, sizeof(textTwo));
-  Serial.println(textTwo);
-  //radio.stopListening();
+  if(!error) {
+    char textTwo[32] = "";
+    radio.read(&textTwo, sizeof(textTwo));
+    Serial.println(textTwo);
+  }
+  radio.stopListening();
 
-  
-  delay(1000);
+  Serial.println();  
+  delay(2000);
   q ++;
-  //id = id > 2 ? 0 : id;
+  id = id == 1 ? 0 : id + 1;
 }
